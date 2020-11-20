253ae688e4ffbfab638fc6cbae64d832a807652d
celiluysal <celiluysl15@gmail.com>
2020-11-20 17:28:06 +0300

    json2mask and mask check done

---
+++ src/deneme.py
@@ -0,0 +1,35 @@
+# Python program to explain cv2.rectangle() method  
+   
+# importing cv2  
+import cv2  
+import  numpy as np
+   
+# path  
+image = np.zeros((500,500, 3), dtype=np.uint8)
+   
+# Window name in which image is displayed 
+window_name = 'Image'
+  
+# Start coordinate, here (5, 5) 
+# represents the top left corner of rectangle 
+start_point = [[5 5]] 
+  
+# Ending coordinate, here (220, 220) 
+# represents the bottom right corner of rectangle 
+end_point =  [[220 220]]
+
+print(type(start_point))
+  
+# # Blue color in BGR 
+# color = (0, 0, 255) 
+  
+# # Line thickness of 2 px 
+# thickness = 2
+  
+# # Using cv2.rectangle() method 
+# # Draw a rectangle with blue line borders of thickness of 2 px 
+# image = cv2.rectangle(image, [[5 5]], [[220 220]], color, thickness) 
+  
+# # Displaying the image  
+# cv2.imshow(window_name, image) 
+# cv2.waitKey(0) 
---
+++ src/json2mask.py
@@ -0,0 +1,35 @@
+import numpy as np
+import cv2
+import json
+import os
+import tqdm
+import re
+
+MASK_DIR  = '../data/masks'
+if not os.path.exists(MASK_DIR):
+    os.mkdir(MASK_DIR)
+
+JSON_DIR = '../data/jsons'
+
+json_list = os.listdir(JSON_DIR)
+json_list.sort()
+
+for json_name in tqdm.tqdm(json_list):
+    json_path = os.path.join(JSON_DIR, json_name)
+    json_file = open(json_path, 'r')
+    json_dict = json.load(json_file)
+
+    mask = np.zeros((json_dict["size"]["height"], json_dict["size"]["width"]), dtype=np.uint8)
+    
+    mask_path = os.path.join(MASK_DIR, json_name[:-5])
+    
+    for obj in json_dict["objects"]:
+        if obj['classTitle']=='Price':
+            # mask = cv2.fillPoly(mask, np.array([obj['points']['exterior']]), color=255)
+            points = np.array(obj['points']['exterior'])
+            points = tuple(map(tuple, points)) 
+            start_point = points[0]
+            end_point = points[1]
+    
+            mask = cv2.rectangle(mask, start_point, end_point, 255, 30) 
+    cv2.imwrite(mask_path, mask.astype(np.uint8))
